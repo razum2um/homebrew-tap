fd55ab8b1d6772049442f4a2d2014066263cf04a
Vlad Bokov <razum2um@mail.ru>
2018-08-12 11:46:06 +0700

    Add ctags+global to search clojure.core/defn

---
+++ Formula/universal-ctags.rb
@@ -0,0 +1,60 @@
+class UniversalCtags < Formula
+  desc "Maintained ctags implementation (incl clojure.core/defn patch)"
+  homepage "https://github.com/razum2um/ctags"
+  head "https://github.com/razum2um/ctags.git"
+  option "without-xml", "Compile without libxml2"
+  option "without-doc", "Compile without man pages"
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "pkg-config" => :build
+  depends_on "jansson" => :optional
+  depends_on "libyaml" => :optional
+  conflicts_with "ctags", :because => "this formula installs the same executable as the ctags formula"
+
+  resource "docutils" do
+    url "https://files.pythonhosted.org/packages/84/f4/5771e41fdf52aabebbadecc9381d11dea0fa34e4759b4071244fa094804c/docutils-0.14.tar.gz"
+    sha256 "51e64ef2ebfb29cae1faa133b3710143496eca21c530f3f71424d77687764274"
+  end
+
+  def install
+    if !build.without? "doc"
+      ENV.prepend_create_path "PYTHONPATH", libexec/"vendor/lib/python2.7/site-packages"
+      ENV.prepend_path "PATH", libexec/"vendor/bin"
+
+      resource("docutils").stage do
+        system "python", *Language::Python.setup_install_args(libexec/"vendor")
+      end
+    end
+
+    opts = []
+    opts << "--disable-xml" if build.without? "xml"
+
+    system "./autogen.sh"
+    system "./configure", "--prefix=#{prefix}", *opts
+    system "make"
+    system "make", "install"
+  end
+
+  def caveats
+    <<~EOS
+      Under some circumstances, emacs and ctags can conflict. By default,
+      emacs provides an executable `ctags` that would conflict with the
+      executable of the same name that ctags provides. To prevent this,
+      Homebrew removes the emacs `ctags` and its manpage before linking.
+      However, if you install emacs with the `--keep-ctags` option, then
+      the `ctags` emacs provides will not be removed. In that case, you
+      won't be able to install ctags successfully. It will build but not
+      link.
+    EOS
+  end
+
+  test do
+    (testpath/"test.clj").write <<~EOS
+      (ns app.controller)
+      (clojure.core/defn core-function-with-body []
+        (println "core"))
+    EOS
+    system "#{bin}/ctags", "-R", "."
+    assert_match /core-function-with-body.*test\.clj/, File.read("tags")
+  end
+end
